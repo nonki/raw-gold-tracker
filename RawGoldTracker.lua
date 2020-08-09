d63bd037d72a9ccb9f170a0ae4f8b993bbea6144
vaequitas <thomas@flack.it>
2020-08-09 14:58:16 +0100

    Use Ace3

---
+++ RawGoldTracker.lua
@@ -0,0 +1,11 @@
+RawGoldTracker = LibStub("AceAddon-3.0"):NewAddon("RawGoldTracker", "AceConsole-3.0")
+
+function RawGoldTracker:OnInitialize()
+    RawGoldTracker:Print("Hello, World!")
+end
+
+function RawGoldTracker:OnEnable()
+end
+
+function RawGoldTracker:OnDisable()
+end

819d94d73ecf04f8763fead1b4c83113119cd9f8
vaequitas <thomas@flack.it>
2020-08-09 15:10:03 +0100

    Use AceConfig

--- RawGoldTracker.lua
+++ RawGoldTracker.lua
@@ -1,7 +1,32 @@
 RawGoldTracker = LibStub("AceAddon-3.0"):NewAddon("RawGoldTracker", "AceConsole-3.0")
 
+function RawGoldTracker:GetMyMessage(info)
+    return 'hi'
+end
+
+function RawGoldTracker:SetMyMessage(info, input)
+    myMessage = input
+end
+
 function RawGoldTracker:OnInitialize()
     RawGoldTracker:Print("Hello, World!")
+
+    local options = {
+        name = "RawGoldTracker",
+        handler = RawGoldTracker,
+        type = 'group',
+        args = {
+            msg = {
+                type = 'input',
+                name = 'My Message',
+                desc = 'The message for my addon',
+                set = 'SetMyMessage',
+                get = 'GetMyMessage',
+            },
+        },
+    }
+
+    LibStub("AceConfig-3.0"):RegisterOptionsTable("RawGoldTracker", options, {"rgt"})
 end
 
 function RawGoldTracker:OnEnable()
